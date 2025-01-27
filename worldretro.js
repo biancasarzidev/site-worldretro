cab34dc79a8249de88366c3d931994a10405a946
Bianca Sarzi <biancasarzidev@gmail.com>
2025-01-27 16:40:31 -0300

    .

--- worldretro.js
+++ worldretro.js
@@ -18,6 +18,7 @@ const totalBanners = banners.length;
 const showBanner = (index) => {
   banners.forEach((banner, i) => {
     banner.classList.remove('active');
+    // Quando o banner é o ativo, ele fica visível
     if (i === index) {
       banner.classList.add('active');
     }
