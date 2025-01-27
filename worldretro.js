578a1ae3f9d28fd68197354e1ce0445ed85c0fb0
Bianca Sarzi <biancasarzidev@gmail.com>
2025-01-27 14:39:54 -0300

    .

--- worldretro.js
+++ worldretro.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.querySelector('.nav-menu');
 
     hamburger.addEventListener('click', () => {
+        
         navMenu.classList.toggle('active');
+
+       
+        hamburger.classList.toggle('active');
     });
 });
